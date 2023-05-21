023dab882bbaca3e9f11ae3b1f485a473f6cbb04
James Jones <jamjon3@gmail.com>
2023-05-21 11:43:11 -0400

    Additional type references and cleanup

--- .github/actions/wine-build/entrypoint.sh
+++ .github/actions/wine-build/entrypoint.sh
@@ -5,10 +5,5 @@ VERSION=$2
 
 /usr/bin/python -m pip install .
 /usr/bin/python build.py
-if [[ "$GITHUB_REF" =~ ^refs/tags.* ]]; then
-  VERSION=$GITHUB_REF_NAME
-else
-  VERSION=$(echo "${GITHUB_REF_NAME}" | sed 's|/|-|g')
-fi
 cd dist
 zip -rm "${BINARY_BASENAME}-${VERSION}-windows-x86_64.zip" "${BINARY_BASENAME}.exe"
