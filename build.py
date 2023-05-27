6af97233af9a623bd5337b6c0e32da5d170049e6
James Jones <jamjon3@gmail.com>
2023-05-27 13:22:50 -0400

    Fixing the windowed issue

--- build.py
+++ build.py
@@ -3,7 +3,7 @@ import PyInstaller.__main__
 PyInstaller.__main__.run([
     'voterwarehouse.py',
     '--onefile',
-    '--windowed',
+    '--nowindowed',
     '--hidden-import=Import.Florida',
     '--hidden-import=Warehouse.Florida',
     '--hidden-import=Import.State',
